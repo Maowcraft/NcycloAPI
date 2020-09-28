cd14903f6516aff25e604c2a0f284cc7e702b558
Maow <28311382+Maowcraft@users.noreply.github.com>
2020-09-28 12:46:10 -0400

    Add files via upload

--- src/main/java/maow/ncycloapi/Ncyclo.java
+++ src/main/java/maow/ncycloapi/Ncyclo.java
@@ -1,5 +1,23 @@
 package maow.ncycloapi;
 
+import maow.ncycloapi.util.ExcludeType;
+
 public class Ncyclo {
     public static final int PORT = 63540;
+    private static final Server server = Server.getInstance();
+
+    @Deprecated
+    public static void send(String data, ExcludeType excludeType, String[] receiverNames) {
+        if (server != null) server.send(data);
+    }
+
+    @Deprecated
+    public static void send(String data, ExcludeType excludeType) {
+        send(data, ExcludeType.NONE, new String[]{});
+    }
+
+    @Deprecated
+    public static void send(String data) {
+        send(data, ExcludeType.NONE);
+    }
 }
